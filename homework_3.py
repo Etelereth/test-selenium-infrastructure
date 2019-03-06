7bd3bd3498d5721bd37666abe37e1f7206d55415
Dmitry Novoselov <mblch@mail.ru>
2019-03-06 11:59:35 +0300

    Homework

--- homework_3.py
+++ homework_3.py
@@ -3,13 +3,17 @@ import sys
 
 driver=webdriver.Chrome()
 driver.get('http://10.77.252.9/litecart/public_html/admin')
-login = driver.find_element_by_name('username')
-login.send_keys('admin')
-pwd = driver.find_element_by_name('password')
-pwd.send_keys('admin')
-enter = driver.find_element_by_name('login')
-enter.click()
-assert 'My Store' in driver.title
-driver.quit()
-print('Page loaded')
+try:
+    login = driver.find_element_by_name('username')
+    login.send_keys('admin')
+    pwd = driver.find_element_by_name('password')
+    pwd.send_keys('admin')
+    enter = driver.find_element_by_name('login')
+    enter.click()
+    assert 'My Store' in driver.title
+    driver.quit()
+    print('Page loaded')
+except Exception as e:
+    print('Что-то пошло не так', e)
+    driver.quit()
 sys.exit()
